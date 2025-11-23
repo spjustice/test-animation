6c8250daecc186a63c9757cba7ae7f6ef8e40db9
spjustice <sanyok.vergun@gmail.com>
2025-11-23 08:53:10 +0400

    anim v1

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   compatibilityDate: '2025-07-15',
+   devtools: { enabled: true },
+   modules: ['@nuxt/eslint', 'v-gsap-nuxt', '@nuxt/ui'],
+   css: ['~/assets/css/main.css'],
+   colorMode: {
+      preference: 'dark',
+      fallback: 'dark',
+      classSuffix: '',
+   },
+});
